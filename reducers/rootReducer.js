f85c5bb9fc38e8f4861fd352e18b561be063a957
robinchoii <robinchoii@gmail.com>
2018-05-03 22:12:32 -0700

    add reducer and initialize store

---
+++ reducers/rootReducer.js
@@ -0,0 +1,25 @@
+const rootReducer = (state, action) => {
+    switch(action.type) {
+
+        case "ADD_RECIPE":
+            return Object.assign({},state, {
+                recipes: state.recipes.concat({ name: action.name })
+            });
+
+        case "ADD_INGREDIENT":
+            const newIngredient = {
+                recipe: action.recipe,
+                name: action.name,
+                quantity: action.quantity
+            };
+
+            return Object.assign({}, state, {
+                ingredients: state.ingredients.concat(newIngredient)
+            });
+
+        return state;
+    };
+}
+
+
+export default rootReducer;
---
+++ store.js
@@ -0,0 +1,13 @@
+import { createStore } from 'redux';
+import rootReducer from './reducers/rootReducer'
+
+const initialState = {
+    recipes: [{name: "Omelette"}],
+    ingredients: [{ recipe: 'Omelette', name: 'Egg', quantity: 2}]
+};
+
+const store = createStore(rootReducer, initialState);
+
+window.store = store;
+
+export default store;
