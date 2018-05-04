0d72e81960714f9dcdf2a3c2025e93077e953f57
robinchoii <robinchoii@gmail.com>
2018-05-04 15:23:33 -0700

    add ui to add recipes

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
 import store from './store';
+import loadUI from './ui';
 import { addRecipe } from './actions/recipes';
 import { addIngredient } from './actions/ingredients';
+
+
+loadUI();
+
 
 store.dispatch(addRecipe('Pancakes'));
 store.dispatch(addIngredient('Pancakes', 'Egg', 3));
---
+++ ui.js
@@ -0,0 +1,42 @@
+import $ from 'jquery';
+import store from './store';
+import { addRecipe } from './actions/recipes';
+
+function updateUI() {
+    const { recipes } = store.getState();
+    // const renderRecipe = (recipe) => `<li>${ recipe.name } </li>`;
+
+    $(".recipes > ul").html(recipes.map( recipe => {
+        return `<li> ${ recipe.name }</li>`;
+    }));
+
+}
+
+function handleAdd() {
+    const $recipeName = $('.recipes > input');
+
+    store.dispatch(addRecipe($recipeName.val()));
+
+    $recipeName.val('');
+}
+
+export default function loadUI() {
+
+    $('#app').append(`
+
+        <div class='recipes'>
+            <h2>Recipes: </h2>
+            <ul></ul>
+            <input type='text' />
+            <button>Add</button>
+        </div>
+
+    `);
+
+    store.subscribe(updateUI);
+
+    $(document).on('click', '.recipes > button', handleAdd);
+
+    updateUI();
+
+};
